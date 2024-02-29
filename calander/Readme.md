0ea65409259a05314f33c0dae977a059dcef9d18
yanjankaf <null@example.com>
2024-02-29 19:29:43 +0530

    First commit

---
+++ calander/Readme.md
@@ -0,0 +1,5 @@
+A Program to print the current months calander in Java.
+
+
+## Learning.
+- first check which year is this, then based on few calculations like - is this century year (% 100), is this leap year ( % 4 ) and then finding the length of each month, and doing unnecessary thing, but thats not necessary at all, computer knows when it is leap yaer - this problem has been solved; so dont solve it again and again; Your computer is capable to tell your local data so ask it.
---
+++ calander/src/Calander.java
@@ -0,0 +1,39 @@
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.Month;
+
+public class Calander {
+    public static void main(String[] args) {
+        // I don't care about user input, so i will just print the calander of the current month
+        LocalDate d = LocalDate.now();
+        int dateToday = d.getDayOfMonth();
+        int currentMonth = d.getMonth().getValue();
+
+        //	Print the header
+
+        System.out.printf("%39s\n", Month.of(currentMonth).toString() + " of " + d.getYear());
+        System.out.println();
+        for (var i : DayOfWeek.values()) {
+            System.out.print(i.toString().substring(0, 3) + " ".repeat(3));
+        }
+        System.out.println();
+
+		LocalDate dayToday = d.minusDays(dateToday - 1);
+		int firstDayOfWeek = dayToday.getDayOfWeek().getValue();
+
+
+		for (int i = 1; i < firstDayOfWeek; ++i){
+            System.out.print(" ".repeat(6));
+        };
+
+        while (dayToday.getMonth().getValue() == currentMonth){
+            System.out.printf("%3s", dayToday.getDayOfMonth());
+            System.out.print(" ".repeat(3));
+            dayToday = dayToday.plusDays(1);
+            if(dayToday.getDayOfWeek().getValue() == 1){
+                System.out.println();
+            }
+        }
+
+    }
+}
